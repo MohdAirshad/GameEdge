e93f28d92a42544030a7c98f52b10c5f93517304
MohdAirshad <105441062+MohdAirshad@users.noreply.github.com>
2023-07-25 14:28:59 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2020 Paul Rybitskyi, paul.rybitskyi.work@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.kotlin.gradle.plugin.KaptExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    gradleVersions()
+    detekt()
+    ktlint()
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+
+    dependencies {
+        classpath(deps.plugins.androidGradle)
+        classpath(deps.plugins.kotlinGradle)
+        classpath(deps.plugins.daggerHiltGradle)
+        classpath(deps.plugins.protobuf)
+        classpath(deps.plugins.gradleVersions)
+    }
+}
+
+detekt {
+    parallel = true
+    buildUponDefaultConfig = true
+    config = files("config/detekt/detekt.yml")
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports.html.required.set(true)
+}
+
+allprojects {
+    apply(plugin = PLUGIN_DETEKT)
+    apply(plugin = PLUGIN_KTLINT)
+
+    repositories {
+        mavenCentral()
+        google()
+        maven { setUrl("https://jitpack.io") }
+    }
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set(versions.ktlint)
+        android.set(true)
+        outputToConsole.set(true)
+        reporters {
+            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.HTML)
+        }
+    }
+
+    // Without the below block, a build failure was happening when
+    // running ./gradlew connectedAndroidTest.
+    // See: https://github.com/mockito/mockito/issues/2007#issuecomment-689365556
+    configurations.all {
+        resolutionStrategy.force("org.objenesis:objenesis:2.6")
+    }
+}
+
+subprojects {
+
+    tasks.withType<KotlinCompile>().all {
+        sourceCompatibility = appConfig.javaCompatibilityVersion.toString()
+        targetCompatibility = appConfig.javaCompatibilityVersion.toString()
+
+        kotlinOptions {
+            freeCompilerArgs += listOf(
+                "-Xopt-in=kotlinx.coroutines.FlowPreview",
+                "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                "-Xopt-in=androidx.compose.material.ExperimentalMaterialApi",
+                "-Xopt-in=androidx.compose.animation.ExperimentalAnimationApi",
+                "-Xopt-in=androidx.compose.ui.ExperimentalComposeUiApi",
+                "-Xopt-in=androidx.compose.foundation.ExperimentalFoundationApi",
+                "-Xopt-in=androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi",
+                "-Xopt-in=com.google.accompanist.pager.ExperimentalPagerApi",
+            )
+
+            jvmTarget = appConfig.kotlinCompatibilityVersion.toString()
+        }
+    }
+
+    plugins.withId(PLUGIN_KOTLIN_KAPT) {
+        extensions.findByType<KaptExtension>()?.run {
+            correctErrorTypes = true
+        }
+    }
+}
+
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+rootProject.name = "Gamedge"
+
+include(":app")
+include(":domain")
+include(":core")
+include(":commons-data")
+include(":commons-ui")
+include(":commons-ui-widgets")
+include(":commons-api")
+include(":commons-testing")
+include(":data")
+include(":database")
+include(":igdb-api")
+include(":igdb-apicalypse")
+include(":gamespot-api")
+include(":feature-category")
+include(":feature-discovery")
+include(":feature-info")
+include(":feature-image-viewer")
+include(":feature-likes")
+include(":feature-news")
+include(":feature-search")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+}
